b74379b50d0fe7b7e3b8c2d25f31f78066a8cc7a
BHyeonKim <rlaqhguse@gmail.com>
2025-12-08 14:26:11 +0900

    init: Initialize nextjs project
    - Use next.js 16.07, react 19.2, @vanilla-extract
    - Use yarn 4.12 and biome

---
+++ .storybook/main.ts
@@ -0,0 +1,26 @@
+import type { StorybookConfig } from '@storybook/nextjs-vite';
+import { vanillaExtractPlugin } from '@vanilla-extract/vite-plugin';
+
+const config: StorybookConfig = {
+  "stories": [
+    "../src/**/*.mdx",
+    "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"
+  ],
+  "addons": [
+    "@chromatic-com/storybook",
+    "@storybook/addon-vitest",
+    "@storybook/addon-a11y",
+    "@storybook/addon-docs",
+    "@storybook/addon-styling-webpack"
+  ],
+  "framework": "@storybook/nextjs-vite",
+  "staticDirs": [
+    "../public"
+  ],
+  async viteFinal(config) {
+    config.plugins = config.plugins || [];
+    config.plugins.push(vanillaExtractPlugin());
+    return config;
+  }
+};
+export default config;

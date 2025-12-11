561dd49bb7a352630400fa2747539677c9085739
BHyeonKim <rlaqhguse@gmail.com>
2025-12-11 21:31:58 +0900

    chore: Add favicons and manifest file

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,6 +8,19 @@ import { bodyStyle } from './layout.css';
 export const metadata: Metadata = {
 	title: 'Runzipper',
 	description: 'Compress files on browser.',
+	manifest: '/site.webmanifest',
+	appleWebApp: {
+		title: 'Runzipper',
+	},
+	icons: {
+		icon: [
+			{ url: '/favicon.ico' },
+			{ url: '/favicon.svg', type: 'image/svg+xml' },
+			{ url: '/favicon-96x96.png', sizes: '96x96', type: 'image/png' },
+		],
+		apple: { url: '/apple-touch-icon.png', sizes: '180x180' },
+		shortcut: '/favicon.ico',
+	},
 };
 
 export default function RootLayout({

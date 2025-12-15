abeda5b79ed499cfaa424224a2e7973c04ab1158
BHyeonKim <rlaqhguse@gmail.com>
2025-12-15 13:03:35 +0900

    feat: Generate localized sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -7,6 +7,19 @@ export default function sitemap(): MetadataRoute.Sitemap {
 			lastModified: new Date(),
 			changeFrequency: 'monthly',
 			priority: 1,
+			alternates: {
+				languages: {
+					ko: 'https://runzipper.app/ko/compress',
+					de: 'https://runzipper.app/de/compress',
+					pl: 'https://runzipper.app/pl/compress',
+					fr: 'https://runzipper.app/fr/compress',
+					it: 'https://runzipper.app/it/compress',
+					ja: 'https://runzipper.app/ja/compress',
+					zh: 'https://runzipper.app/zh/compress',
+					es: 'https://runzipper.app/es/compress',
+					en: 'https://runzipper.app/en/compress',
+				},
+			},
 		},
 	];
 }
